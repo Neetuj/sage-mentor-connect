19d375fe3ca2967cc4b5f5abab62be85d3064415
Neetu Jain <nutshi@gmail.com>
2025-08-04 11:05:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sage-mentor-connect/",    // <-- Add this line
   server: {
     host: "::",
     port: 8080,
